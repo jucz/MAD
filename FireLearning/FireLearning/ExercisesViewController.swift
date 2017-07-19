d569fdd3ce77284e6d8549a03fe8cf399031fe74
Hendrik <h.plitzner@gmx.de>
2017-07-19 13:19:15 +0200

    hallo?

---
+++ FireLearning/FireLearning/ExercisesViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ExercisesViewController.swift
+//  FireLearning
+//
+//  Created by Admin on 19.07.17.
+//  Copyright © 2017 HS Osnabrueck. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    var data = [String]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 0...20 {
+            data.append("aufgabe\(i)")
+        }
+        
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+        
+        let text = data[indexPath.row] //2.
+        
+        cell.textLabel?.text = text //3.
+        
+        return cell //4.
+    }
+}
